a8c814075a676de5c27b08ff3d8f30443561c904
cjlathin <56983077+cjlathin@users.noreply.github.com>
2025-11-05 15:28:22 -0700

    Refactor GET function to use response variables

--- src/routes/api/fetch_players_info/+server.js
+++ src/routes/api/fetch_players_info/+server.js
@@ -5,11 +5,12 @@ import { json, error } from '@sveltejs/kit';
 
 export async function GET() {
     // get NFL state from sleeper (week and year)
-	const [nflState, leagueData, playoffs] = await waitForAll(
+    const [nflStateRes, leagueDataRes, playoffsRes] = await waitForAll(
         fetch(`https://api.sleeper.app/v1/state/nfl`, {compress: true}),
         fetch(`https://api.sleeper.app/v1/league/${leagueID}`, {compress: true}),
         fetch(`https://api.sleeper.app/v1/league/${leagueID}/winners_bracket`, {compress: true}),
     )
+    
     const [nflState, leagueData, playoffs] = await waitForAll(
         nflStateRes.json(),
         leagueDataRes.json(),
@@ -19,8 +20,7 @@ export async function GET() {
 	let year = nflState.league_season;
     const regularSeasonLength = leagueData.settings.playoff_week_start - 1;
     const playoffLength = playoffs.pop().r;
-	const fullSeasonLength = regularSeasonLength + playoffLength;
-
+    const fullSeasonLength = regularSeasonLength + playoffLength;
 
     const resPromises = [
         fetch(`https://api.sleeper.app/v1/players/nfl`, {compress: true})
